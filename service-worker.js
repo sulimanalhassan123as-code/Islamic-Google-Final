b3f602dc9c7b79e48047ec08c34513230a55dc4a
sulimanalhassan123as-code <sulimanalhassan.123.as@gmail.com>
2025-10-12 14:16:33 +0000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("islamic-google-cache").then(cache => {
+      return cache.addAll(["/", "/index.html", "/css/styles.css", "/js/app.js"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
